30b04a4bcdc5df53fd045e766d47e2b10e37a209
sya-ri <sya79lua@gmail.com>
2021-07-13 09:47:05 +0900

    add v0.3.0 options - zip_compression_iterations - work_around_minecraft_quirks - zip_spec_conformance_level - size_increasing_zip_obfuscation - percentage_of_zip_structures_tuned_for_obfuscation_discretion - never_store_squash_times - transcode_ogg - minify_json - delete_bloat_keys - color_quantization_target - maximum_width_and_height - minify_shader - minify_properties

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,22 +15,35 @@ if [ -n "$COMPRESS_ALREADY_COMPRESSED_FILES" ]; then
 else
     RECOMPRESS_COMPRESSED_FILES=${6}
 fi
-IGNORE_SYSTEM_AND_HIDDEN_FILES=${7}
-ALLOW_MOD_OPTIFINE=${8}
+ZIP_COMPRESSION_ITERATIONS=${7}
+WORK_AROUND_MINECRAFT_QUIRKS=${8}
+IGNORE_SYSTEM_AND_HIDDEN_FILES=${9}
+ALLOW_MOD_OPTIFINE=${10}
 ALLOW_MODS="["
 if [ "$ALLOW_MOD_OPTIFINE" = "true" ]; then
     ALLOW_MODS="$ALLOW_MODS'OptiFine'"
 fi
 ALLOW_MODS="$ALLOW_MODS]"
-SAMPLING_FREQUENCY=${9}
-TARGET_PITCH=${10}
-MINIMUM_BITRATE=${11}
-MAXIMUM_BITRATE=${12}
-QUANTIZE_IMAGE=${13}
+ZIP_SPEC_CONFORMANCE_LEVEL=${11}
+SIZE_INCREASING_ZIP_OBFUSCATION=${12}
+PERCENTAGE_OF_ZIP_STRUCTURES_TUNED_FOR_OBFUSCATION_DISCRETION=${13}
+NEVER_STORE_SQUASH_TIMES=${14}
+TRANSCODE_OGG=${15}
+SAMPLING_FREQUENCY=${16}
+TARGET_PITCH=${17}
+MINIMUM_BITRATE=${18}
+MAXIMUM_BITRATE=${19}
+MINIFY_JSON=${20}
+DELETE_BLOAT_KEYS=${21}
+QUANTIZE_IMAGE=${22}
 if [ -n "$QUANTIZE_IMAGE" ]; then
     echo '::error ::quantize_image: Removed in v0.3.0. Delete this option.'
 fi
-OUTPUT=${14}
+COLOR_QUANTIZATION_TARGET=${23}
+MAXIMUM_WIDTH_AND_HEIGHT=${24}
+MINIFY_SHADER=${25}
+MINIFY_PROPERTIES=${26}
+OUTPUT=${27}
 
 # print version
 printf 'PackSquash version: ' && packsquash --version
@@ -44,9 +57,18 @@ if [ -z "$SETTING_FILE" ]; then
 pack_directory = '$DIRECTORY_PATH'
 skip_pack_icon = $SKIP_PACK_ICON
 recompress_compressed_files = $RECOMPRESS_COMPRESSED_FILES
+zip_compression_iterations = $ZIP_COMPRESSION_ITERATIONS
+work_around_minecraft_quirks = $WORK_AROUND_MINECRAFT_QUIRKS
 ignore_system_and_hidden_files = $IGNORE_SYSTEM_AND_HIDDEN_FILES
 allow_mods = $ALLOW_MODS
+zip_spec_conformance_level = '$ZIP_SPEC_CONFORMANCE_LEVEL'
+size_increasing_zip_obfuscation = $SIZE_INCREASING_ZIP_OBFUSCATION
+percentage_of_zip_structures_tuned_for_obfuscation_discretion = $PERCENTAGE_OF_ZIP_STRUCTURES_TUNED_FOR_OBFUSCATION_DISCRETION
+never_store_squash_times = $NEVER_STORE_SQUASH_TIMES
 output_file_path = '$OUTPUT'
+
+['**/*?.ogg']
+transcode_ogg = $TRANSCODE_OGG
 
 ['**/*.{og[ga],mp3,wav,flac}']
 sampling_frequency = $SAMPLING_FREQUENCY
@@ -54,7 +76,20 @@ minimum_bitrate = $MINIMUM_BITRATE
 maximum_bitrate = $MAXIMUM_BITRATE
 target_pitch = $TARGET_PITCH
 
+['**/*.{json,jsonc}']
+minify_json = $MINIFY_JSON
+delete_bloat_keys = $DELETE_BLOAT_KEYS
+
 ['**/*.png']
+color_quantization_target = '$COLOR_QUANTIZATION_TARGET'
+maximum_width_and_height = $MAXIMUM_WIDTH_AND_HEIGHT
+
+['**/*.{fsh,vsh}']
+minify_shader = $MINIFY_SHADER
+
+['**/*.properties']
+minify_properties = $MINIFY_PROPERTIES
+
 " > packsquash-settings.toml
 
   SETTING_FILE=packsquash-settings.toml
