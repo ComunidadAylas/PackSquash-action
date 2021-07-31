c9a9713b50c796044a39b07be5a8162b5562ad5d
sya-ri <sya79lua@gmail.com>
2021-08-01 07:00:58 +0900

    feat: add a parameter to set PackSquash version

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,7 @@ RUN apt-get update \
  && apt-get install -y --no-install-recommends \
     curl \
     ca-certificates \
+    jq \
     wget \
     unzip \
     git \
@@ -16,15 +17,6 @@ RUN apt-get update \
     gstreamer1.0-plugins-bad \
  && apt-get -y clean \
  && rm -rf /var/lib/apt/lists/*
-
-RUN curl -sSL https://api.github.com/repos/ComunidadAylas/PackSquash/releases/tags/v0.3.0-rc.1 \
- | grep "browser_download_url.*PackSquash.executable.Linux.x64.glibc.zip\"" \
- | cut -d : -f 2,3 \
- | tr -d \" \
- | wget -qi -\
- && unzip PackSquash.executable.Linux.x64.glibc \
- && chmod a+x packsquash \
- && mv packsquash /usr/local/bin/
 
 COPY entrypoint.sh /entrypoint.sh
 COPY git-set-file-times.sh /git-set-file-times.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -17,9 +17,49 @@ if [ -n "$INPUT_QUANTIZE_IMAGE" ]; then
     echo '::error ::quantize_image: Removed in v0.3.0. Delete this option.'
 fi
 
+case "$INPUT_VERSION" in
+  'latest' )
+    if [ -z $GITHUB_TOKEN ]; then
+      echo '::error::Environment variable GITHUB_TOKEN is not set.'
+      exit 1
+    else
+      curl -sSL https://api.github.com/repos/ComunidadAylas/PackSquash/actions/artifacts \
+       | jq ".artifacts | sort_by(.created_at) | reverse" \
+       | jq "map(select(.name == \"PackSquash executable (Linux, x64, glibc)\"))" \
+       | jq -r ".[0] | .url" \
+       | (echo `cat`/'zip') \
+       | wget --header="Authorization: token $GITHUB_TOKEN" -qi -
+       unzip zip
+    fi
+  ;;
+  'v0.1.0' | 'v0.1.1' | 'v0.1.2' | 'v0.2.0' | 'v0.2.1' )
+    if [ -z $INPUT_SETTINGS_FILE ]; then
+      echo '::error::Not using settings_file in older versions is not supported.'
+      exit 1
+    else
+      curl -sSL "https://api.github.com/repos/ComunidadAylas/PackSquash/releases/tags/$INPUT_VERSION" \
+       | grep "browser_download_url.*PackSquash.executable.Linux.zip\"" \
+       | cut -d : -f 2,3 \
+       | tr -d \" \
+       | wget -qi -
+      unzip PackSquash.executable.Linux
+    fi
+  ;;
+  * ) # v0.3.0-rc.1
+    curl -sSL "https://api.github.com/repos/ComunidadAylas/PackSquash/releases/tags/$INPUT_VERSION" \
+     | grep "browser_download_url.*PackSquash.executable.Linux.x64.glibc.zip\"" \
+     | cut -d : -f 2,3 \
+     | tr -d \" \
+     | wget -qi -
+    unzip PackSquash.executable.Linux.x64.glibc
+  ;;
+esac
+
+chmod a+x packsquash
+
 # print version
 echo "::group::PackSquash version:"
-packsquash --version
+./packsquash --version
 echo "::endgroup::"
 
 # change to GitHub WorkSpace Directory
@@ -106,7 +146,7 @@ fi
 /git-set-file-times.sh
 
 # optimize
-packsquash packsquash-settings.toml
+./packsquash packsquash-settings.toml
 
 # save optimized pack
 cp "$INPUT_OUTPUT_PATH" "$INPUT_CACHE_PATH/cache_pack.zip"
