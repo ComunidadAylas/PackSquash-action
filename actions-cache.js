e50743e4aef5271b6efc9b0e13ee0d6ea6837a74
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2021-08-16 17:23:55 +0200

    fix: test fix for cache not being able to be updated

--- actions-cache.js
+++ actions-cache.js
@@ -1,12 +1,13 @@
 const cache = require('@actions/cache')
 const cached_file_paths = ['pack.zip', 'system_id', 'packsquash-options.toml']
 const cache_key = `packsquash-zip-${process.env.GITHUB_WORKFLOW}`
+const unique_cache_key = `${cache_key}-${Date.now()}`
 
 async function run() {
     if (process.argv[2] === 'save') {
-        await cache.saveCache(cached_file_paths, cache_key)
+        await cache.saveCache(cached_file_paths, unique_cache_key)
     } else {
-        await cache.restoreCache(cached_file_paths, cache_key)
+        await cache.restoreCache(cached_file_paths, unique_cache_key, [cache_key])
     }
 }
 
