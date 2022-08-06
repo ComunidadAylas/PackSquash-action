4cd1b772b23c4cafe347d9b86d1a091668a2c266
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2022-08-06 16:21:07 +0200

    fix(git_set_file_times): do not fail when there are no files. Refactors
    When git log is called on changeTime, it may return empty output if
    there are no files to get the time of. Moreover, due to a GitHub Actions
    toolkit bug, output.stdout is undefined in this case.
    
    To avoid special-casing that in changeTime and avoid that bug, let's
    ignore repositories with no files in ls in the first place, which is
    also more efficient.

--- src/git_set_file_times.ts
+++ src/git_set_file_times.ts
@@ -12,7 +12,9 @@ interface Repository {
 }
 
 async function setGitFileModificationTimes(workspace: string, pack_directory: string) {
+    debug(`Setting file modification times according to commit history for ${pack_directory}`);
     return getSubmodulePaths(workspace).then(async submodules => {
+        debug(`Detected submodules: ${submodules}`);
         const workspaces = [workspace, ...submodules];
         const repositories = await ls(workspace, workspaces, pack_directory);
         return await changeTime(repositories);
@@ -20,57 +22,66 @@ async function setGitFileModificationTimes(workspace: string, pack_directory: st
 }
 
 async function ls(root_workspace: string, workspaces: string[], pack_directory: string): Promise<Repository[]> {
-    return Promise.all(
-        workspaces
-            .map(workspace => {
-                const name = path.relative(root_workspace, workspace);
-                const directory = path.relative(workspace, pack_directory) || '.';
-                if (directory.includes('..')) {
-                    debug(`The candidate workspace ${name}/ is outside the pack_directory, ignoring it`);
-                    return null;
-                }
-                return getExecOutput('git', ['-C', workspace, 'ls-files', '-z', directory], {
-                    silent: true
-                }).then(output => ({
-                    name: name,
-                    workspace: workspace,
-                    files: new Set(
-                        output.stdout.split('\n').flatMap(line =>
-                            line
-                                .split('\0')
-                                .filter(f => !!f)
-                                .map(f => path.join(workspace, f))
-                        )
-                    ),
-                    directory: directory
-                }));
-            })
-            .filter(<T>(item: T | null): item is T => item !== null)
+    const repositories = await Promise.all(
+        workspaces.map(async workspace => {
+            const name = path.relative(root_workspace, workspace);
+            const directory = path.relative(workspace, pack_directory);
+            if (directory.includes('..')) {
+                debug(`The pack directory is outside the candidate workspace ${name}/, ignoring it`);
+                return null;
+            }
+
+            const output = await getExecOutput('git', ['-C', workspace, 'ls-files', '-z', directory], {
+                silent: true
+            });
+            const files = new Set(
+                output.stdout.split('\n').flatMap(line =>
+                    line
+                        .split('\0')
+                        .filter(f => !!f)
+                        .map(f => path.join(workspace, f))
+                )
+            );
+
+            // Guard against no files to prevent git log later outputting nothing in changeTime
+            if (files.size == 0) {
+                debug(`There are no pack files in the candidate workspace ${name}/, ignoring it`);
+                return null;
+            }
+
+            return {
+                name: name,
+                workspace: workspace,
+                files: files,
+                directory: directory
+            };
+        })
     );
+
+    return repositories.filter((repository): repository is Repository => repository !== null);
 }
 
 async function changeTime(repositories: Repository[]) {
     await Promise.all(
-        repositories.map(({ name, workspace, files, directory }) => {
+        repositories.map(async ({ name, workspace, files, directory }) => {
             debug(`Setting the modification time for ${files.size} files in ${name}/`);
-            return getExecOutput('git', ['-C', workspace, 'log', '-m', '-r', '--name-only', '--no-color', '--pretty=format:%ct', '-z', directory], {
+            const output = await getExecOutput('git', ['-C', workspace, 'log', '-m', '-r', '--name-only', '--no-color', '--pretty=format:%ct', '-z', directory], {
                 silent: true
-            }).then(async output => {
-                for (const line of output.stdout.split('\0\0')) {
-                    const [rawTime, rawFiles] = line.split('\n');
-                    const time = new Date(parseInt(rawTime) * 1000);
-                    for (const file of rawFiles.split('\0').map(f => path.join(workspace, f))) {
-                        if (files.size === 0) {
-                            return;
-                        }
-                        if (files.delete(file)) {
-                            await utimes(file, time, time).catch(() => {
-                                // Ignore
-                            });
-                        }
+            });
+            for (const line of output.stdout.split('\0\0')) {
+                const [rawTime, rawFiles] = line.split('\n');
+                const time = new Date(parseInt(rawTime) * 1000);
+                for (const file of rawFiles.split('\0').map(f => path.join(workspace, f))) {
+                    if (files.delete(file)) {
+                        await utimes(file, time, time).catch(() => {
+                            // Ignore
+                        });
+                    }
+                    if (files.size === 0) {
+                        return;
                     }
                 }
-            });
+            }
         })
     );
 }
