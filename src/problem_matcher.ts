27304376d81141e6f3b3b4db8416a2b39c921e53
AlexTMjugador <AlexTMjugador@users.noreply.github.com>
2022-10-11 18:41:31 +0200

    fix(problem_matcher): continue working around quirks

--- src/problem_matcher.ts
+++ src/problem_matcher.ts
@@ -7,6 +7,13 @@ const json = {
             owner: 'packsquash-error',
             severity: 'error',
             pattern: [
+                // Workaround for https://github.com/actions/runner/blob/44d4d076fec3fddaf68c876cbea2217110d48892/src/Runner.Worker/IssueMatcher.cs#L449-L453
+                {
+                    regexp: '^.*$'
+                },
+                // message can't be set by several patterns, and when looping, it can only be set by the looping pattern.
+                // Work around that by matching error start lines as continuation lines, with the downside of considering
+                // consecutive errors as if they were a single error
                 {
                     regexp: '^(?:\x1B\\[\\d+\\w*)*(?:[!❌] | {2,3})(.+?)(?:\x1B\\[0m)?$',
                     message: 1,
@@ -18,6 +25,13 @@ const json = {
             owner: 'packsquash-warning',
             severity: 'warning',
             pattern: [
+                // Workaround for https://github.com/actions/runner/blob/44d4d076fec3fddaf68c876cbea2217110d48892/src/Runner.Worker/IssueMatcher.cs#L449-L453
+                {
+                    regexp: '^.*$'
+                },
+                // message can't be set by several patterns, and when looping, it can only be set by the looping pattern.
+                // Work around that by matching error start lines as continuation lines, with the downside of considering
+                // consecutive errors as if they were a single error
                 {
                     regexp: '^(?:\x1B\\[\\d+\\w*)*(?:[*⚡] | {2,3})(.+?)(?:\x1B\\[0m)?$',
                     message: 1,
